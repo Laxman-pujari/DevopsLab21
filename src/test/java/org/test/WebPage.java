39144e8fca49672c1122a27e2541697d60eb622f
2NR23CS001 <2NR23CS001@gmail.com>
2026-03-07 11:31:52 +0530

    Added Test file and jar plugin

---
+++ src/test/java/org/test/WebPage.java
@@ -0,0 +1,36 @@
+package org.test ;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class WebPage {
+
+    WebDriver driver;
+
+    // Runs before test starts
+    @BeforeClass
+    public void setup() throws  InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.get("https://laxman-pujari.github.io/DevopsLab21/");
+    }
+
+    // Actual test
+    @Test
+    public void testWebPageTitle() {
+        String title = driver.getTitle();
+        System.out.println("Page Title: " + title);
+        Assert.assertEquals(title, "Data Engineer Roadmap");
+    }
+
+    // Runs after test finishes
+    @AfterClass
+    public void tearDown() throws InterruptedException{
+        Thread.sleep(1000);
+        driver.quit();
+    }
+}
